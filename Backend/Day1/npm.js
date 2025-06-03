760f9352d1e559c53482e8d0fc41c7f7666bf1c1
Mruganksha <kudakemruganksha@gmail.com>
2025-06-04 00:11:26 +0530

    completed npm basics

---
+++ Backend/Day1/npm.js
@@ -0,0 +1,16 @@
+//NPM understanding:
+//npm - (1st it was node package manager) 
+//node.js core mein jo installed aata hai that is module
+//npm se download krte a that is package
+
+//installing and uninstalling anything basics and advanced
+// to install => npm install packageName
+// uninstall => npm uninstall packageName
+// to install particular version : npm i accessibility@package version
+//example: npm i accessibility@3.0.9
+
+//understanding node_modules
+//dependencies :  packages and dependencies of packages
+// devdependencies : packages which depend or are used only in development
+
+//scripts - understanding default scripts PATH and custom scripts
