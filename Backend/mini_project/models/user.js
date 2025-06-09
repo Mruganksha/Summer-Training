a4153a4fdfb16f98638a53414521c95ed3ee0797
Mruganksha <kudakemruganksha@gmail.com>
2025-06-09 22:59:45 +0530

    completed the password generation part

---
+++ Backend/mini_project/models/user.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+
+mongoose.set("strictQuery", true);
+
+mongoose.connect("mongodb://127.0.0.1:27017");
+
+const userSchema = mongoose.Schema({
+    username: String,
+    name: String,
+    age: Number,
+    email: String,
+    password: String,
+    post: [
+       {type: mongoose.Schema.Types.ObjectId,
+        ref: "post"
+       },
+    ]
+})
+
+module.exports = mongoose.model('user', userSchema);
